2c53c07358d0bf0e66ad96c4e5ca07f146f2311a
whitefoxblue <46203656+whitefoxblue@users.noreply.github.com>
2018-12-27 18:48:14 -0700

    Casual day has gone too far.

---
+++ Dilbert.py
@@ -0,0 +1,14 @@
+class Lion:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return self.name
+
+
+#lion = Lion("Dilbert")
+#print(lion)
+
+lion = Lion("Dilbert")
+print(lion)
+
